cd4252fcaafc1cb501932b411f26083cd77b5d5a
gqueste <gabriel.queste@gmail.com>
2015-08-11 22:53:41 +0200

    Pooling

--- back/database.js
+++ back/database.js
@@ -3,14 +3,14 @@ module.exports = {
 	connection: function(){
 		if(process.env.CLEARDB_DATABASE_URL){
 			var address = process.env.CLEARDB_DATABASE_URL.toString();
-			var tab1 = address.parse('://');
-			var tab2 = tab1[1].parse(':');
+			var tab1 = address.split('://');
+			var tab2 = tab1[1].split(':');
 			var user = tab2[0];
-			var tab3 = tab2[1].parse('@');
+			var tab3 = tab2[1].split('@');
 			var password = tab3[0];
-			var tab4 = tab3[1].parse('/');
+			var tab4 = tab3[1].split('/');
 			var host = tab4[0];
-			var tab5 = tab4[1].parse('?');
+			var tab5 = tab4[1].split('?');
 			var database = tab5[0];
 
 
