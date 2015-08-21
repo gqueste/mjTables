8d75cf62b0d7ddf227929eb6e51906b2c5baf573
gqueste <gabriel.queste@gmail.com>
2015-08-21 14:25:10 +0100

    Front : Remove dodgy calls when add or remove a player

--- front/app/scripts/components/table/table_controller.js
+++ front/app/scripts/components/table/table_controller.js
@@ -97,8 +97,10 @@ angular.module('mjTables').
 
         $scope.removePlayer = function(user_id){
             TableAPI.removePlayerFromTable($scope.tableid, user_id).then(function(){
-                var message = 'Un joueur a bien été retiré de la table.';
-                init(null, message);
+                if($scope.refreshOnPlayerMovement){
+                    var message = 'Un joueur a bien été retiré de la table.';
+                    init(null, message);
+                }
                 $rootScope.$broadcast('playerRemoved', {table_id : $scope.tableid, user_id : user_id});
             }).catch(function(error){
                 console.log(error);
@@ -107,8 +109,10 @@ angular.module('mjTables').
 
         $scope.rejoindreTable = function(){
             TableAPI.addPlayerToTable($scope.tableid, ConnexionService.getCurrentUserId()).then(function(){
-                var message = 'Un joueur a bien été ajouté.';
-                init(null, message);
+                if($scope.refreshOnPlayerMovement){
+                    var message = 'Un joueur a bien été ajouté.';
+                    init(null, message);
+                }
                 $rootScope.$broadcast('playerAdded', {table_id : $scope.tableid, user_id : ConnexionService.getCurrentUserId()});
             }).catch(function(error){
                 console.log(error);
